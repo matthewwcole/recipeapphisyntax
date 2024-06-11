2732fb5e7aa94989679edec6b85f77f0d32cb8aa
Matthew Cole <mr.mwcole@gmail.com>
2024-06-11 16:18:20 -0400

    first commit on practicing UI

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:recipeapphisyntax/constants/images_path.dart';
 import 'package:recipeapphisyntax/screens/home.dart';
+import 'package:recipeapphisyntax/theme/pallete.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
@@ -10,85 +11,82 @@ class OnBoardingScreen extends StatelessWidget {
     final h = MediaQuery.of(context).size.height;
     final w = MediaQuery.of(context).size.width;
 
-    return Scaffold(
-      body: SizedBox(
-        height: h,
-        width: w,
-        child: Stack(
-          children: [
-            Positioned(
-                top: 0,
-                child: Container(
-                  height: h * .79,
-                  width: w,
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage(
-                            ImagesPath.onBoarding,
-                          ),
-                          fit: BoxFit.cover)),
-                )),
-            Center(
-              child: Image.asset(ImagesPath.onBoardingtitle),
-            ),
-            Positioned(
+    return SafeArea(
+      child: Scaffold(
+        body: SizedBox(
+          height: h,
+          width: w,
+          child: Stack(
+            children: [
+              Positioned(
+                  top: 0,
+                  child: Container(
+                    height: h * .79,
+                    width: w,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                              ImagesPath.onBoarding,
+                            ),
+                            fit: BoxFit.cover)),
+                  )),
+              Center(
+                child: Image.asset(ImagesPath.onBoardingtitle),
+              ),
+              Positioned(
                 bottom: 0,
                 child: Container(
-                  height: h * .243,
+                  height: h * .250,
                   width: w,
                   decoration: const BoxDecoration(
-                    color: Colors.white,
+                    color: Pallete.whiteColor,
                     borderRadius: BorderRadius.only(
                         topRight: Radius.circular(40),
                         topLeft: Radius.circular(40)),
                   ),
-                  child: Padding(
-                    padding: EdgeInsets.only(top: h * .032),
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            "It's Kraut'n Time!",
-                            style: TextStyle(
-                              fontSize: w * .06,
-                              fontWeight: FontWeight.w600,
-                            ),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "It's Kraut'n Time!",
+                          style: TextStyle(
+                            fontSize: w * .06,
+                            fontWeight: FontWeight.w600,
                           ),
-                          SizedBox(
-                            height: h * .01,
-                          ),
-                          const Text(
-                            'The Secret is the Kraut',
-                            style: TextStyle(
-                                fontSize: 14, fontWeight: FontWeight.w300),
-                          ),
-                          SizedBox(
-                            height: h * .032,
-                          ),
-                          SizedBox(
-                            width: w * .8,
-                            child: ElevatedButton(
-                                onPressed: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => const Home()));
-                                },
-                                child: const Text(
-                                  "Get Kraut'n",
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.bold),
-                                )),
-                          )
-                        ],
-                      ),
+                        ),
+                        SizedBox(height: h * .01),
+                        const Text(
+                          'The Secret is the Kraut',
+                          style: TextStyle(
+                              fontSize: 14, fontWeight: FontWeight.w300),
+                        ),
+                        SizedBox(height: h * .032),
+                        SizedBox(
+                          width: w * .8,
+                          child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => const Home()));
+                              },
+                              child: const Text(
+                                "Get Kraut'n",
+                                style: TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              )),
+                        )
+                      ],
                     ),
                   ),
-                ))
-          ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
